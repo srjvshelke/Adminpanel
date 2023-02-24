267455cf1a1106b430e0e6eba237356912f0c581
suraj <surajshelkeofficial@gmail.com>
2023-02-24 07:58:34 +0530

    topbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./Scenes/global/Topbar";
+// import Sidebar from "./Scenes/global/Sidebar";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
-    <div className="App">
-Hello Admin
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        {/* <Sidebar isSidebar={isSidebar} /> */}
+        <main className="content">
+          <Topbar/>
+        </main>
+      </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
   );
 }
 
